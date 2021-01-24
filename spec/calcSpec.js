dc5270642f4d787742565b7e7efc2ef34ed1d820
George <glychock@gmail.com>
2021-01-23 20:08:55 -0500

    scripts: Added sample tests from lesson

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,7 +1,14 @@
 describe("Calculator", function() {
     describe("Addition tests", function() {
+
         it("should return 42", function() {
             expect(addition(20, 22)).toBe(42);
+        });
+
+        it("should return an error if we don't supply two numbers", function() {
+            spyOn(window, "alert");
+            addition("Hitchhikers", "Guide");
+            expect(window.alert).toHaveBeenCalledWith("Error!");
         });
     });
 });
