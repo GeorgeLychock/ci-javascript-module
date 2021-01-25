beea8ce82074527c392d4136ae7940d48f61d7b7
George <glychock@gmail.com>
2021-01-24 22:25:48 -0500

    scripts: Finished Drink About challenge

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,9 +1,17 @@
 function whatCanIDrink(age) {
-    if (typeof(age) == "number" && (age < 0)) {
+    if (typeof (age) == "number" && (age <= 0)) {
         return ("Sorry. I can’t tell what drink because that age is incorrect!");
-    } else if (typeof(age) == "number" && (age < 14)) {
+    } else if (typeof (age) == "number" && (age < 14)) {
         return ("Drink Toddy");
-    } 
- }
+    } else if (typeof (age) == "number" && (age < 18) && (age >= 14)) {
+        return ("Drink Coke");
+    } else if (typeof (age) == "number" && (age < 21) && (age >= 18)) {
+        return ("Drink Beer");
+    } else if (typeof (age) == "number" && (age < 130) && (age >= 21)) {
+        return ("Drink Whisky");
+    } else {
+        return ("Sorry. I can’t tell what drink because that age is incorrect!");
+    }
+}
 
 
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -5,8 +5,24 @@ describe("DrinkAbout", function() {
             expect(whatCanIDrink(-2)).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
         });
 
-        it("should return Drink Toddy if age is more than 1 and less than 14", function() {
-            expect(whatCanIDrink(13)).toBe("Drink Toddy");
+        it("should return Drink Toddy if age is 1 or more and less than 14", function() {
+            expect(whatCanIDrink(7)).toBe("Drink Toddy");
+        });
+
+        it("should return Drink Coke if age is 14 or higher and less than 18", function() {
+            expect(whatCanIDrink(15)).toBe("Drink Coke");
+        });
+
+        it("should return Drink Beer if age is 18 or higher and less than 21", function() {
+            expect(whatCanIDrink(19)).toBe("Drink Beer");
+        });
+
+        it("should return Drink Whisky if age is 21 or higher and less than 130", function() {
+            expect(whatCanIDrink(35)).toBe("Drink Whisky");
+        });
+
+        it("should return error that age is not a valid age", function() {
+            expect(whatCanIDrink(137)).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
         });
 
     });
