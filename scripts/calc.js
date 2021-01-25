036b663f02ccb956fb86cd9dcbd687d752fca7fe
George <glychock@gmail.com>
2021-01-25 18:55:08 -0500

    scripts: Finished FizzBuzz challenge

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,16 +1,15 @@
-function whatCanIDrink(age) {
-    if (typeof (age) == "number" && (age <= 0)) {
-        return ("Sorry. I can’t tell what drink because that age is incorrect!");
-    } else if (typeof (age) == "number" && (age < 14)) {
-        return ("Drink Toddy");
-    } else if (typeof (age) == "number" && (age < 18) && (age >= 14)) {
-        return ("Drink Coke");
-    } else if (typeof (age) == "number" && (age < 21) && (age >= 18)) {
-        return ("Drink Beer");
-    } else if (typeof (age) == "number" && (age < 130) && (age >= 21)) {
-        return ("Drink Whisky");
+
+function fizzBuzz(entry) {
+    if (typeof (entry) == "number" && (entry < 0)) {
+        return ("Sorry. you must supply a valid number!");
+    } else if (entry % 3 === 0 && entry % 5 === 0) {
+        return "FizzBuzz";
+    } else if (entry % 3 === 0) {
+        return "Fizz";
+    } else if (entry % 5 === 0) {
+        return "Buzz";
     } else {
-        return ("Sorry. I can’t tell what drink because that age is incorrect!");
+        return entry;
     }
 }
 
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,28 +1,20 @@
-describe("DrinkAbout", function() {
-    describe("Age tests", function() {
+describe("FizzBuzz", function() {
+    describe("Entry Tests", function() {
 
-        it("should return error that age is not a valid age", function() {
-            expect(whatCanIDrink(-2)).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+        it("should return error that entry is not a valid number", function() {
+            expect(fizzBuzz(-2)).toBe("Sorry. you must supply a valid number!");
         });
-
-        it("should return Drink Toddy if age is 1 or more and less than 14", function() {
-            expect(whatCanIDrink(7)).toBe("Drink Toddy");
+        it("should return FizzBuzz if entry is divisible by 3 and 5", function() {
+            expect(fizzBuzz(15)).toBe("FizzBuzz");
         });
-
-        it("should return Drink Coke if age is 14 or higher and less than 18", function() {
-            expect(whatCanIDrink(15)).toBe("Drink Coke");
+        it("should return Fizz if entry is divisible by 3", function() {
+            expect(fizzBuzz(27)).toBe("Fizz");
         });
-
-        it("should return Drink Beer if age is 18 or higher and less than 21", function() {
-            expect(whatCanIDrink(19)).toBe("Drink Beer");
+        it("should return Fizz if entry is divisible by 5", function() {
+            expect(fizzBuzz(25)).toBe("Buzz");
         });
-
-        it("should return Drink Whisky if age is 21 or higher and less than 130", function() {
-            expect(whatCanIDrink(35)).toBe("Drink Whisky");
-        });
-
-        it("should return error that age is not a valid age", function() {
-            expect(whatCanIDrink(137)).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+        it("should return Entry if entry is not divisible by 3 or 5", function() {
+            expect(fizzBuzz(29)).toBeDefined();
         });
 
     });
